a410af8c189a643c83687df3e0bb0229242aae76
b3b3r <b3b3rseg@gmail.com>
2020-10-01 16:10:50 +0200

    create flags for CLI

--- main.go
+++ main.go
@@ -1,5 +1,42 @@
+// To tun it : go run main.go -src img/ -dst output -task channel -poolsize 5
+
 package main
 
-func main() {
+import (
+	"flag"
+	"fmt"
+	"time"
+	"udemy/imgproc/task"
 
+	"udemy/imgproc/filter"
+)
+
+func main() {
+	srcDir := flag.String("src", "", "Input directory")
+	dstDir := flag.String("dst", "", "Output directory")
+	filterType := flag.String("filter", "grayscale", "grayscale/blur")
+	taskType := flag.String("task", "channel", "channel/group")
+	poolSize := flag.Int("poolsize", 4, "Workers pool size for channel task")
+	flag.Parse()
+
+	var f filter.Filter
+	switch *filterType {
+	case "grayscale":
+		f = filter.GrayScale{}
+	case "blur":
+		f = filter.Blur{}
+	}
+
+	var t task.Tasker
+	switch *taskType {
+	case "channel":
+		t = task.NewChanTask(*srcDir, *dstDir, f, *poolSize)
+	case "group":
+		t = task.NewWaitGrpTask(*srcDir, *dstDir, f)
+	}
+
+	start := time.Now()
+	t.Process()
+	elapsed := time.Since(start)
+	fmt.Printf("Time elapsed: %s\n", elapsed)
 }
